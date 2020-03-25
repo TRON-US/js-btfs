64a2adcbec3fc4b312d012493f015777b4673ec9
uchenna <uchenna.anyanwu@tron.network>
2020-03-25 12:58:10 -0700

    [BTFS-1437] - added gogo dep

--- packages/btfs-http-client/dloaddeps.sh
+++ packages/btfs-http-client/dloaddeps.sh
@@ -4,6 +4,6 @@
 node ./src/downloader.js
 
 #fix text in the file
-sed -i.bk 's|../../github.com/tron-us/protobuf/gogoproto/|../|g' ./js-btfs-common/master/js/protos/guard/guard_pb.js
-sed -i.bk 's|../../github.com/tron-us/protobuf/gogoproto/|../|g' ./js-btfs-common/master/js/protos/ledger/ledger_pb.js
-sed -i.bk 's|../../github.com/tron-us/protobuf/gogoproto/|../|g' ./js-btfs-common/master/js/protos/escrow/escrow_pb.js
+sed -i.bk 's|../../github.com/tron-us/protobuf/gogoproto/|../gogo/|g' ./js-btfs-common/master/js/protos/guard/guard_pb.js
+sed -i.bk 's|../../github.com/tron-us/protobuf/gogoproto/|../gogo/|g' ./js-btfs-common/master/js/protos/ledger/ledger_pb.js
+sed -i.bk 's|../../github.com/tron-us/protobuf/gogoproto/|../gogo/|g' ./js-btfs-common/master/js/protos/escrow/escrow_pb.js
